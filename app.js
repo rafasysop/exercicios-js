826029bb63c8fed3546589eca56b2412d4e08e88
Rafael Moura <rafaelcostamoura@gmail.com>
2022-09-15 20:01:10 -0300

    response 1 and 2

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
   Dica: pesquise por remainder operator (%).
 */
 
-const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5];
+
+const oddNumbers = randomNumbers.filter((number) => number % 2 !== 0);
+console.log("response 1: ", oddNumbers);
 
 /*
   02
@@ -15,7 +18,14 @@ const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
   - Exiba no console quantos números abaixo de 501 o array abaixo possui.
 */
 
-const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691];
+
+const quantityNumberUnder501 = crazyNumbers.filter((number) => number < 501);
+console.log(
+  "response 2: ",
+  `${quantityNumberUnder501.length} numbers: `,
+  quantityNumberUnder501.sort((a, b) => a - b)
+);
 
 /*
   03
@@ -26,7 +36,7 @@ const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
   Dica: pesquise por exponentiation operator (**).
 */
 
-const numbers = [5, 7, 3]
+const numbers = [5, 7, 3];
 
 /*
   04
@@ -37,17 +47,17 @@ const numbers = [5, 7, 3]
 */
 
 const tarantinoMovies = [
-  { name: 'Bastardos inglórios', release: 2009 },
-  { name: 'Pulp Fiction', release: 1994 },
-  { name: 'Kill Bill: Volume 2', release: 2004 },
-  { name: 'Quatro Quartos', release: 1995 },
-  { name: 'Sin City', release: 2005 },
-  { name: 'Era uma Vez em... Hollywood', release: 2019 },
-  { name: 'Django Livre', release: 2012 },
-  { name: 'Cães de Aluguel', release: 1992 },
-  { name: 'À Prova de Morte', release: 2007 },
-  { name: 'Kill Bill: Volume 1', release: 2003 }
-]
+  { name: "Bastardos inglórios", release: 2009 },
+  { name: "Pulp Fiction", release: 1994 },
+  { name: "Kill Bill: Volume 2", release: 2004 },
+  { name: "Quatro Quartos", release: 1995 },
+  { name: "Sin City", release: 2005 },
+  { name: "Era uma Vez em... Hollywood", release: 2019 },
+  { name: "Django Livre", release: 2012 },
+  { name: "Cães de Aluguel", release: 1992 },
+  { name: "À Prova de Morte", release: 2007 },
+  { name: "Kill Bill: Volume 1", release: 2003 },
+];
 
 /*
   05
@@ -57,14 +67,14 @@ const tarantinoMovies = [
 */
 
 const tvShows = [
-  { name: 'Breaking Bad', releaseYear: 2008 },
-  { name: 'Mr. Robot', releaseYear: 2015 },
-  { name: 'True Detective', releaseYear: 2014 },
-  { name: 'Hannibal', releaseYear: 2013 },
-  { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
-  { name: 'House M.D.', releaseYear: 2004 },
-  { name: 'Watchmen', releaseYear: 2019 }
-]
+  { name: "Breaking Bad", releaseYear: 2008 },
+  { name: "Mr. Robot", releaseYear: 2015 },
+  { name: "True Detective", releaseYear: 2014 },
+  { name: "Hannibal", releaseYear: 2013 },
+  { name: "The Handmaid's Tale", releaseYear: 2017 },
+  { name: "House M.D.", releaseYear: 2004 },
+  { name: "Watchmen", releaseYear: 2019 },
+];
 
 /*
   06
@@ -78,12 +88,12 @@ const tvShows = [
 */
 
 const cart = [
-  { name: 'Dark Souls III', price: 95.03 },
-  { name: 'Shadow of the Tomb Raider', price: 101.19 },
-  { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
-  { name: 'Resident Evil 2', price: 119.90 },
-  { name: 'Death Stranding', price: 149.99 }
-]
+  { name: "Dark Souls III", price: 95.03 },
+  { name: "Shadow of the Tomb Raider", price: 101.19 },
+  { name: "Sekiro: Shadows Die Twice", price: 179.99 },
+  { name: "Resident Evil 2", price: 119.9 },
+  { name: "Death Stranding", price: 149.99 },
+];
 
 /*
 - Nome 1
